95a6ddc80a936d11ec7ca2f3cc74a9fb32977b8d
Edward Schults <edward.schults@gmail.com>
2018-03-15 10:17:59 +0100

    end of lecture

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,16 @@
 class Movie < ApplicationRecord
   belongs_to :director
+
+  include PgSearch
+
+  multisearchable against: [ :title, :syllabus ]
+
+  # pg_search_scope :search_by_title_and_syllabus,
+  #   against: [ :title, :syllabus ],
+  #   associated_against: {
+  #     director: [ :first_name, :last_name ]
+  #   },
+  #   using: {
+  #     tsearch: { prefix: true, highlight: true }
+  #   }
 end
